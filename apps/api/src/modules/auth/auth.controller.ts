b5e9419a341bf6a86681bd216b928fcf10dcebdd
Vinicius Souza <viniciussouza_bm@hotmail.com>
2026-05-08 23:25:51 -0300

    adiciona current user decorator

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+} from '@nestjs/common'
 
 import { AuthService } from './auth.service'
 
 import { RegisterDto } from './dto/register.dto'
 import { LoginDto } from './dto/login.dto'
+
+import { JwtAuthGuard } from './guards/jwt-auth.guard'
+import { CurrentUser } from './decorators/current-user.decorator'
 
 @Controller('auth')
 export class AuthController {
@@ -17,5 +26,11 @@ export class AuthController {
   @Post('login')
   async login(@Body() dto: LoginDto) {
     return this.authService.login(dto)
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async me(@CurrentUser() user: any) {
+    return user
   }
 }
