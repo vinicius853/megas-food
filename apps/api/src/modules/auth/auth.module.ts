ca068a21c6c5bd8512674bbfc708bdecb898ade8
Vinicius Souza <viniciussouza_bm@hotmail.com>
2026-05-08 23:08:29 -0300

    adiciona jwt strategy e auth guard

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { PassportModule } from '@nestjs/passport'
 
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
+import { JwtStrategy } from './strategies/jwt.strategy'
 
 @Module({
   imports: [
     ConfigModule,
+
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
 
     JwtModule.registerAsync({
       inject: [ConfigService],
@@ -21,7 +27,9 @@ import { AuthService } from './auth.service'
   ],
 
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService, JwtModule],
+
+  providers: [AuthService, JwtStrategy],
+
+  exports: [AuthService, JwtModule, PassportModule],
 })
 export class AuthModule {}
